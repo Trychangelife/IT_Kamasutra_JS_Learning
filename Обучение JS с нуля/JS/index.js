b31c6b3591ecee52783056654e8bed1b2d23d563
Trychangelife <zkonstantinov@bk.ru>
2022-03-28 23:41:11 +0700

    Functional

--- Обучение JS с нуля/JS/index.js
+++ Обучение JS с нуля/JS/index.js
@@ -1,5 +1,5 @@
-var firstNameEl;
 var firstNameId;
+var firstNameEl;
 var lastNameId;
 var lastNameEl;
 var addresEl;
@@ -13,10 +13,19 @@ var avatarId = 'avatar-wrapper';
 var avatarEl = document.getElementById(avatarId);
 
 
+function alertValue() {
+    console.log(citiesEl.value);
+    console.log(textAreaEl.value);
+    console.log(avatarEl.innerHTML);
+    console.log(firstNameEl.value);
+    console.log(firstNameEl.className);
+    console.log(addresEl.value);
+    console.log(lastNameEl.className);
+}
+
+
 textAreaId = 'hobbies';
 textAreaEl = document.getElementById(textAreaId);
-
-
 firstNameId = 'first-name';
 firstNameEl = document.getElementById(firstNameId);
 addresId = 'address';
@@ -25,17 +34,9 @@ lastNameId = 'last-name';
 lastNameEl = document.getElementById(lastNameId);
 citiesId = 'cities';
 citiesEl = document.getElementById(citiesId);
-//window.alert(citiesEl.value);
-//window.alert(textAreaEl.value);
-//window.alert(avatarEl.innerHTML);
-//window.alert(firstNameEl.value);
-//window.alert(firstNameEl.className);
-//window.alert(addresEl.value);
-
-
 firstNameEl.value = 'Evegeniy';
-window.alert(lastNameEl.className);
 lastNameEl.className = 'error-input';
 avatarEl.src = 'https://miro.medium.com/max/1400/0*6e2K0U6ZkN9Ju9iL.jpg';
 citiesEl.value = 'Minsk';
 textAreaEl.value = 'I have not interest';
+alertValue();
