92e6f777660b99e7eb867186c6102b01840d2530
Trychangelife <zkonstantinov@bk.ru>
2022-03-29 00:17:52 +0700

    Update index.js

--- Обучение JS с нуля/JS/index.js
+++ Обучение JS с нуля/JS/index.js
@@ -1,42 +1,37 @@
-var firstNameId;
-var firstNameEl;
-var lastNameId;
-var lastNameEl;
-var addresEl;
-var addresId;
-var citiesId;
-var citiesEl;
-var textAreaEl;
-var textAreaId;
+function alertValues(Param) {
+    console.log(Param.value);
+    console.log(Param.className);
+}
+
+
+
+var firstNameId = 'first-name';
+var firstNameEl = document.getElementById(firstNameId);
+alertValues(firstNameEl);
+
+var lastNameId = 'last-name';
+var lastNameEl = document.getElementById(lastNameId);
+alertValues(lastNameEl);
+
+var addresId = 'address';
+var addresEl = document.getElementById(addresId);
+alertValues(addresEl);
+
+var citiesId = 'cities';
+var citiesEl = document.getElementById(citiesId);
+alertValues(citiesEl);
+
+var textAreaId = 'hobbies';
+var textAreaEl = document.getElementById(textAreaId);
+alertValues(textAreaEl);
+
 var image
 var avatarId = 'avatar-wrapper';
 var avatarEl = document.getElementById(avatarId);
 
 
-function alertValue() {
-    console.log(citiesEl.value);
-    console.log(textAreaEl.value);
-    console.log(avatarEl.innerHTML);
-    console.log(firstNameEl.value);
-    console.log(firstNameEl.className);
-    console.log(addresEl.value);
-    console.log(lastNameEl.className);
-}
-
-
-textAreaId = 'hobbies';
-textAreaEl = document.getElementById(textAreaId);
-firstNameId = 'first-name';
-firstNameEl = document.getElementById(firstNameId);
-addresId = 'address';
-addresEl = document.getElementById(addresId);
-lastNameId = 'last-name';
-lastNameEl = document.getElementById(lastNameId);
-citiesId = 'cities';
-citiesEl = document.getElementById(citiesId);
 firstNameEl.value = 'Evegeniy';
 lastNameEl.className = 'error-input';
 avatarEl.src = 'https://miro.medium.com/max/1400/0*6e2K0U6ZkN9Ju9iL.jpg';
 citiesEl.value = 'Minsk';
 textAreaEl.value = 'I have not interest';
-alertValue();
