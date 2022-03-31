cc91816b07e49e13cc6de78c049825299fea039f
Trychangelife <zkonstantinov@bk.ru>
2022-03-31 23:45:20 +0700

    Update Объявление объекта (ключ лист).js

--- JS FCC/Объявление объекта (ключ лист).js
+++ JS FCC/Объявление объекта (ключ лист).js
@@ -7,3 +7,15 @@ const myDog = {
   
     // Only change code above this line
   };
+
+
+  // Доступ к свойствам объекта с помощью записи через точку 
+const testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+  };
+  
+  // Only change code below this line
+  const hatValue = testObj.hat;      // Change this line
+  const shirtValue = testObj.shirt;    // Change this line
