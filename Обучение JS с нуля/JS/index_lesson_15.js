906561f824f8c52f0178829de5f41163ca671ede
Trychangelife <zkonstantinov@bk.ru>
2022-04-02 17:02:39 +0700

    Update index_lesson_15.js

--- Обучение JS с нуля/JS/index_lesson_15.js
+++ Обучение JS с нуля/JS/index_lesson_15.js
@@ -6,50 +6,32 @@ let buttonDevide = document.getElementById('buttonDevide');
 let input1 = document.getElementById("number1");
 let input2 = document.getElementById("number2");
 
-function getNumber1() {
-    return Number(input1.value);
-}
-function getNumber2() {
-    return Number(input2.value);
-}
-
 function makeOperation(operationCode) {
+
+    var number1 = Number(input1.value);
+    var number2 = Number(input2.value);
+
+
     if (operationCode === '+') {
-        var result = getNumber1() + getNumber2();
+        var result = number1 + number2;
     } else if (operationCode === '-') {
-        var result = getNumber1() - getNumber2();
+        var result = number1 - number2;
     } else if (operationCode === '*') {
-        var result = getNumber1() * getNumber2();
+        var result = number1 * number2;
     } else if (operationCode === '/') {
-        var result = getNumber1() / getNumber2();
-        
+        var result = number1 / number2;
+
     }
-
     window.alert(result);
-    
 }
 
-
-function onButtonPlusClick() {
-    makeOperation('+');
+function onOperationButtonClick(eventObject) {
+    var clickedElement = eventObject.currentTarget;
+    var operation = clickedElement.innerHTML;
+    makeOperation(operation);
 }
-
-function onButtonMunusClick() {
-    makeOperation('-');
-
-}
-
-function onButtonMultiplyClick() {
-    makeOperation('*');
-}
-
-function onButtonDevideClick() {
-    makeOperation('/');
-}
-
-
-buttonPlus.addEventListener('click', onButtonPlusClick);
-buttonMunus.addEventListener('click', onButtonMunusClick);
-buttonMultiply.addEventListener('click', onButtonMultiplyClick);
-buttonDevide.addEventListener('click', onButtonDevideClick);
+buttonPlus.addEventListener('click', onOperationButtonClick);
+buttonMunus.addEventListener('click', onOperationButtonClick);
+buttonMultiply.addEventListener('click', onOperationButtonClick);
+buttonDevide.addEventListener('click', onOperationButtonClick);
 
