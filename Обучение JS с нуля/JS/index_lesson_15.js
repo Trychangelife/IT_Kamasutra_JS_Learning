810c901bf2956d71446e8f53ec5e46d8e52622a6
Trychangelife <zkonstantinov@bk.ru>
2022-04-02 16:46:17 +0700

    Update index_lesson_15.js

--- Обучение JS с нуля/JS/index_lesson_15.js
+++ Обучение JS с нуля/JS/index_lesson_15.js
@@ -3,49 +3,48 @@ let buttonMunus = document.getElementById('buttonMinus');
 let buttonMultiply = document.getElementById('buttonMultiply');
 let buttonDevide = document.getElementById('buttonDevide');
 
+let input1 = document.getElementById("number1");
+let input2 = document.getElementById("number2");
+
+function getNumber1() {
+    return Number(input1.value);
+}
+function getNumber2() {
+    return Number(input2.value);
+}
+
+function makeOperation(operationCode) {
+    if (operationCode === '+') {
+        var result = getNumber1() + getNumber2();
+    } else if (operationCode === '-') {
+        var result = getNumber1() - getNumber2();
+    } else if (operationCode === '*') {
+        var result = getNumber1() * getNumber2();
+    } else if (operationCode === '/') {
+        var result = getNumber1() / getNumber2();
+        
+    }
+
+    window.alert(result);
+    
+}
+
+
 function onButtonPlusClick() {
-    let input1 = document.getElementById("number1");
-    let input2 = document.getElementById("number2");
-
-    let number1 = Number(input1.value);
-    let number2 = Number(input2.value);
-
-    let summ = number1 + number2;
-    window.alert(summ);
+    makeOperation('+');
 }
 
 function onButtonMunusClick() {
-    let input1 = document.getElementById("number1");
-    let input2 = document.getElementById("number2");
-
-    let number1 = Number(input1.value);
-    let number2 = Number(input2.value);
-
-    let summ = number1 - number2;
-    window.alert(summ);
+    makeOperation('-');
 
 }
 
 function onButtonMultiplyClick() {
-    let input1 = document.getElementById("number1");
-    let input2 = document.getElementById("number2");
-
-    let number1 = Number(input1.value);
-    let number2 = Number(input2.value);
-
-    let summ = number1 * number2;
-    window.alert(summ);
+    makeOperation('*');
 }
 
 function onButtonDevideClick() {
-    let input1 = document.getElementById("number1");
-    let input2 = document.getElementById("number2");
-
-    let number1 = Number(input1.value);
-    let number2 = Number(input2.value);
-
-    let summ = number1 / number2;
-    window.alert(summ);
+    makeOperation('/');
 }
 
 
