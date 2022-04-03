238a11605ee2d37e0d4d8a409b05f9d7f6420883
Trychangelife <zkonstantinov@bk.ru>
2022-04-03 12:49:56 +0700

    Update DNA to RNA.js

--- CodeWars/JS/DNA to RNA.js
+++ CodeWars/JS/DNA to RNA.js
@@ -11,3 +11,10 @@ function DNAtoRNA(dna) {
     })
     return result.join('');
   }
+
+
+//   Нормальное решение через регулярное выражение 
+//   function DNAtoRNA(dna) {
+//     // create a function which returns an RNA sequence from the given DNA sequence
+//     return dna.replace(/T/g, 'U');
+//   }
