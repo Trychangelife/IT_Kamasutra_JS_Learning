a26e7053a342c60a2732923179fc1a612b6b6dee
Trychangelife <zkonstantinov@bk.ru>
2022-04-03 22:20:40 +0700

    Create List filtering.js

---
+++ CodeWars/JS/7kyu/List filtering.js
@@ -0,0 +1,12 @@
+function filter_list(l) {
+    // Return a new array with the strings filtered out
+    var arr = l.filter(element => typeof element === 'number');
+        return arr;
+  }
+
+
+
+  for (let index = 0; index < array.length; index++) {
+      const element = array[index];
+      object
+  }
