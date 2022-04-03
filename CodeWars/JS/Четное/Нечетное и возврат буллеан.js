baaff560ca5eb12f6147529aacd374a6b74986fc
Trychangelife <zkonstantinov@bk.ru>
2022-04-03 13:12:17 +0700

    Create Нечетное и возврат буллеан.js

---
+++ CodeWars/JS/Четное/Нечетное и возврат буллеан.js
@@ -0,0 +1,10 @@
+.function lovefunc(flower1, flower2){
+    // moment of truth
+    var total = (flower1 + flower2) % 2;
+    if (total > 0) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
