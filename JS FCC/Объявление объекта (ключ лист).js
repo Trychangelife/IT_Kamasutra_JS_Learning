5e03deebdd4f4caf4ea72c6329c148856cb31f7c
Trychangelife <zkonstantinov@bk.ru>
2022-04-05 22:52:48 +0700

    Update Объявление объекта (ключ лист).js

--- JS FCC/Объявление объекта (ключ лист).js
+++ JS FCC/Объявление объекта (ключ лист).js
@@ -112,4 +112,17 @@ const myDog = {
 
 
 
+  const myStorage = {
+    "car": {
+      "inside": {
+        "glove box": "maps",
+        "passenger seat": "crumbs"
+       },
+      "outside": {
+        "trunk": "jack"
+      }
+    }
+  };
   
+  const gloveBoxContents = myStorage.car.inside['glove box'];
+
