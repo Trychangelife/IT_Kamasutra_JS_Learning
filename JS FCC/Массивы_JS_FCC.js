aa23e066fbb7f0b3579c32c5b45f7ba80a1d9642
Trychangelife <zkonstantinov@bk.ru>
2022-04-07 23:33:40 +0700

    Update Массивы_JS_FCC.js

--- JS FCC/Массивы_JS_FCC.js
+++ JS FCC/Массивы_JS_FCC.js
@@ -74,3 +74,29 @@ function sum(arr, n) {
     // Only change code above this line
   }
   
+
+
+
+  function randomFraction(n) {
+
+    // Only change code below this line
+  
+    return Math.random(n);
+  
+    // Only change code above this line
+  }
+
+  function randomWholeNum() {
+
+    // Only change code below this line
+  
+    return Math.floor(Math.random() * 10);
+  }
+
+
+
+  function randomRange(myMin, myMax) {
+    // Only change code below this line
+    return Math.floor(Math.random() * (myMax - myMin + 1) + myMin);
+    // Only change code above this line
+  }
