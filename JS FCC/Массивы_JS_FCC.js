5495df2dd2cac4c6320c82c970a62402dc718705
Trychangelife <zkonstantinov@bk.ru>
2022-04-07 23:44:20 +0700

    Update Массивы_JS_FCC.js

--- JS FCC/Массивы_JS_FCC.js
+++ JS FCC/Массивы_JS_FCC.js
@@ -118,3 +118,20 @@ function convertToInteger(str) {
   }
   
   convertToInteger("10011");
+
+
+
+  function checkEqual(a, b) {
+    return a === b ? 'Equal' : 'Not Equal';
+  }
+  
+  checkEqual(1, 2);
+
+
+
+
+  function checkSign(num) {
+    return (num > 0) ? 'positive' : (num === 0) ? 'zero' : 'negative';
+  }
+  
+  checkSign(10);
