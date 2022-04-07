699ce8a0bf1c13213a16d03eb183d3b302057565
Trychangelife <zkonstantinov@bk.ru>
2022-04-07 19:22:16 +0700

    Update Массивы_JS_FCC.js

--- JS FCC/Массивы_JS_FCC.js
+++ JS FCC/Массивы_JS_FCC.js
@@ -15,3 +15,12 @@ for (let i = 1; i < 6; i++) {
   myArray.push(i);
 }
 // Only change code below this line
+
+// Setup
+const myArray = [];
+for (let i = 1; i < 10; i +=2) {
+  myArray.push(i);
+}
+
+console.log(myArray);
+// Only change code below this line
