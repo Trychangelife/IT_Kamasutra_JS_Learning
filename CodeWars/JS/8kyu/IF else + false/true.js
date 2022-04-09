fff8fe3be8a24534f12592bba198917e8efe95b1
Trychangelife <zkonstantinov@bk.ru>
2022-04-09 09:10:48 +0700

    Create true.js

---
+++ CodeWars/JS/8kyu/IF else + false/true.js
@@ -0,0 +1,14 @@
+function setAlarm(employed, vacation){
+    if (employed === true && vacation === true) {
+      return false;
+    }
+    else if (employed === false && vacation === true) {
+      return false;
+    }
+    else if (employed === false && vacation === false) {
+      return false;
+    }
+    else {
+      return true;
+    }
+  }
