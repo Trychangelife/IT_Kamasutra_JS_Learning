6775cdc3a88ec885b34581e8025bfafa2498fb3e
Trychangelife <zkonstantinov@bk.ru>
2022-04-10 07:36:57 +0700

    Update Create Phone Number.js

--- CodeWars/JS/7kyu/Create Phone Number.js
+++ CodeWars/JS/7kyu/Create Phone Number.js
@@ -3,3 +3,19 @@ function createPhoneNumber(n){
     return '\(' + str[0].concat(n[1]).concat(n[2]) + '\)' + " " + str[6].concat(n[4]).concat(n[5]) + '-' 
               + str[12].concat(n[7]).concat(n[8]).concat(n[9]);
 }
+
+
+
+
+// Как решили нормальные люди )
+
+// function createPhoneNumber(numbers){
+//     var format = "(xxx) xxx-xxxx";
+    
+//     for(var i = 0; i < numbers.length; i++)
+//     {
+//       format = format.replace('x', numbers[i]);
+//     }
+    
+//     return format;
+//   }
