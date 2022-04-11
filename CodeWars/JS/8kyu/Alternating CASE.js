04ea3c25f463a72ada77005e19e5a791f6809ae6
Trychangelife <zkonstantinov@bk.ru>
2022-04-11 21:47:14 +0700

    Create Alternating CASE.js

---
+++ CodeWars/JS/8kyu/Alternating CASE.js
@@ -0,0 +1,13 @@
+String.prototype.toAlternatingCase = function () {
+    // Define your method here :)
+    let str = '';
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === this[i].toUpperCase()) {
+        str += this[i].toLowerCase();
+      }
+      else { 
+        str += this[i].toUpperCase();
+      }
+    }
+    return str;
+  }
