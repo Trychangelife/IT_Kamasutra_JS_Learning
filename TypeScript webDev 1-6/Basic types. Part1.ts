09e020df81ee9a9bb4d422d398e11f3ca2688701
Trychangelife <zkonstantinov@bk.ru>
2022-04-12 22:55:15 +0700

    Lesson 1

---
+++ TypeScript webDev 1-6/Basic types. Part1.ts
@@ -0,0 +1,8 @@
+typeof 42; //'number'
+typeof 'str'; //'string'
+typeof true; //'boolean'
+typeof []; //'object'
+typeof {}; //'object'
+typeof null; //'object'
+typeof undefined; //'undefined'
+typeof Symbol(); // 'symbol'
