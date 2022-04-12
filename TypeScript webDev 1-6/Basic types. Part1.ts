f31d5e2ff32d585f7f66194beddfba9e0f2163a1
Trychangelife <zkonstantinov@bk.ru>
2022-04-13 00:07:44 +0700

    Lesson 1

--- TypeScript webDev 1-6/Basic types. Part1.ts
+++ TypeScript webDev 1-6/Basic types. Part1.ts
@@ -8,10 +8,38 @@ typeof undefined; //'undefined'
 typeof Symbol(); // 'symbol'
 
 // const
-const num = '42';
+const nums = '42';
 // Сменить тип уже не получится, будет ошибка.
 
 //let
 let num = 42;
 num = 'hello'; //Ошибки нет - тип переменной изменен
+
+//Boolean Type
+let isCompleted: boolean = false;
+// Изменить тип в настоящий момент НЕЛЬЗЯ
+
+isCompleted = true; //Но можно изменить значение в буле
+
+//Number Type
+const decimal: number = 6;
+const integer: number = 7.10;
+const hex: number = 0xf00d;
+const binary: number = 0b1010;
+const octal: number = 0o744;
+
+// String Type
+const name1: string = 'Yauhen';
+
+// TS type
+const u: undefined = undefined;
+const u: null = null; //В JS это тип 'object' - в этом различие
+
+//Void Type (Тип VOID пишется ПОСЛЕ скобочек) - Обозначает, то что функция НЕ возвращает что-либо (нет команды Return)
+const greetUser = (): void => {
+    alert("Hello, nice to see you!")
+};
+
+
+
 
