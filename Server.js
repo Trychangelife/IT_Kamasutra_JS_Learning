800611b577b03611ed104960994942c9be9c80cf
Trychangelife <zkonstantinov@bk.ru>
2022-04-14 12:55:49 +0700

    Create Server.js

---
+++ Server.js
@@ -0,0 +1,21 @@
+const http = require('http')
+let requestsCount = 0
+
+const server = http.createServer((request, response) => {
+    requestsCount++
+
+    switch (request.url) {
+        case '/students':
+            response.write('STUDENTS')
+            break;
+            case '/courses':
+            response.write('FRONT + BACK')
+            break;
+        default:
+            response.write('404 NOT FOUND')
+    }
+    response.write(' IT-KAMASUTRA' + requestsCount)
+    response.end()
+})
+
+server.listen(3003)
