d522802b5e6ecaaeadc1af6cc37162cc0085b81a
Trychangelife <zkonstantinov@bk.ru>
2022-04-14 23:59:06 +0700

    Try change Faveicon

--- project/Server.js
+++ project/Server.js
@@ -1,6 +1,20 @@
 const http = require('http')
 let requestsCount = 0
 
+// Import modules
+const favicon = require('serve-favicon');
+const express = require('express')
+const app = express()
+
+// Returns a middleware to serve favicon
+app.use(favicon('D:/Учеба/WebStorm/project/favicon.ico'));
+
+// API endpoint to serve index
+app.get('/', (_, res)=> res.sendFile('D:/Учеба/WebStorm/project/index.html'))
+
+// Start the server
+app.listen(3000);
+
 const server = http.createServer((request, response) => {
     requestsCount++
 
@@ -8,7 +22,7 @@ const server = http.createServer((request, response) => {
         case '/students':
             response.write('STUDENTS')
             break;
-            case '/courses':
+        case '/courses':
             response.write('FRONT + BACK')
             break;
         default:
