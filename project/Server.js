a420ad6af21ab9511814ebb310f6d127e3916021
Trychangelife <zkonstantinov@bk.ru>
2022-04-15 18:29:31 +0700

    При помощи Express

--- project/Server.js
+++ project/Server.js
@@ -1,35 +1,22 @@
-const http = require('http')
-let requestsCount = 0
+var app = require('express')();
+var http = require('http').Server(app);
+let count = 0
 
-// Import modules
-const favicon = require('serve-favicon');
-const express = require('express')
-const app = express()
+app.get('/', function (req, res){
 
-// Returns a middleware to serve favicon
-app.use(favicon('D:/Учеба/WebStorm/project/favicon.ico'));
+    count++
 
-// API endpoint to serve index
-app.get('/', (_, res)=> res.sendFile('D:/Учеба/WebStorm/project/index.html'))
-
-// Start the server
-app.listen(3000);
-
-const server = http.createServer((request, response) => {
-    requestsCount++
-
-    switch (request.url) {
-        case '/students':
-            response.write('STUDENTS')
-            break;
-        case '/courses':
-            response.write('FRONT + BACK')
-            break;
-        default:
-            response.write('404 NOT FOUND')
+    if (req.url === '/1') {
+        res.write('students')
+        res.end()
     }
-    response.write(' IT-KAMASUTRA' + requestsCount)
-    response.end()
+    else {
+        res.write("404 NOT FOUND" + count)
+    }
+    res.sendFile(__dirname, '/public/index.html')
+    res.end()
 })
 
-server.listen(3003)
+http.listen(3000, function (){
+    console.log('HTTP server started on port 3000')
+})
