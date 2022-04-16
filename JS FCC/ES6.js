45deb6719c3844acfc57e886619414c857e08e7b
Trychangelife <zkonstantinov@bk.ru>
2022-04-17 01:17:48 +0700

    Update ES6.js

--- JS FCC/ES6.js
+++ JS FCC/ES6.js
@@ -39,3 +39,9 @@ function freezeObj() {
   };
   
   console.log(myConcat([1, 2], [3, 4, 5]));
+
+  // 
+
+  // Only change code below this line
+const increment = (number, value = 1) => number + value;
+// Only change code above this line
