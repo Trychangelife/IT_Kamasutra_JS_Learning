a44578d81df39c5a5c03b79e9a9ab1c211209360
Trychangelife <zkonstantinov@bk.ru>
2022-04-17 01:14:14 +0700

    Update ES6.js

--- JS FCC/ES6.js
+++ JS FCC/ES6.js
@@ -30,3 +30,12 @@ function freezeObj() {
   const magic = () => {
     return new Date();
   };
+
+
+  // Сложение массивов с синтаксическим сахаром
+
+  const myConcat = (arr1, arr2) =>  {
+    return arr1.concat(arr2);
+  };
+  
+  console.log(myConcat([1, 2], [3, 4, 5]));
