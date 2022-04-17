383ee6d91fc7d7be5e5cceebaf2be7aa99a4e43e
Trychangelife <zkonstantinov@bk.ru>
2022-04-17 12:52:25 +0700

    17.04

--- JS FCC/ES6.js
+++ JS FCC/ES6.js
@@ -55,3 +55,31 @@ const HIGH_TEMPERATURES = {
 };
 
 const {today, tomorrow} = HIGH_TEMPERATURES;
+
+
+
+//
+
+const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+};
+
+// Only change code below this line
+
+const {today: highToday, tomorrow: highTomorrow} = HIGH_TEMPERATURES;
+// Only change code above this line
+
+
+//
+const LOCAL_FORECAST = {
+    yesterday: { low: 61, high: 75 },
+    today: { low: 64, high: 77 },
+    tomorrow: { low: 68, high: 80 }
+};
+
+// Only change code below this line
+const { today: { low: lowToday, high: highToday } } = LOCAL_FORECAST;
+
+// Only change code above this line
