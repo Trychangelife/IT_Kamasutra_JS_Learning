a9c93ecba714a6164be0926a8f71ff2264defa5c
Trychangelife <zkonstantinov@bk.ru>
2022-05-24 23:10:42 +0700

    Create what is between.js

---
+++ CodeWars/TypeScript/what is between.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.between = void 0;
+function between(a, b) {
+    const arr = [];
+    for (let i = a; i <= b; i++) {
+        arr.push(i);
+    }
+    return arr;
+}
+exports.between = between;
+//# sourceMappingURL=what%20is%20between.js.map
